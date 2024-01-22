daea55e3681553e918fd0a71fee868441ddc6e2b
Sudhir Das <sudhirdas1999@gmail.com>
2024-01-22 14:39:32 +0530

    added: 3 sorting based (recursion)

---
+++ Recursion/src/RecursionSorting.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class RecursionSorting {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 6, 3, 2, 1, 7, 8, 10, 9};
+        bubble(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+        selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr, int c, int r) {
+        if (r == 0) {
+            return;
+        }
+
+        if (c < r) {
+            if (arr[c] > arr[c + 1]) {
+                //swap
+                int temp = arr[c];
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
+            }
+            bubble(arr, c + 1, r);
+        } else {
+            bubble(arr, 0, r-1);
+        }
+    }
+
+    static void selection(int[] arr, int r, int c, int max){
+        if (r == 0){
+            return;
+        }
+
+        if(c < r){
+            if(arr[c] > arr[max]){
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
+            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+
+            selection(arr, r-1, 0, 0);
+        }
+    }
+}
--- Recursion/src/arrays/findTargetIndex.java
+++ Recursion/src/arrays/findTargetIndex.java
@@ -1,11 +1,21 @@
 package arrays;
+
+import java.util.ArrayList;
+
 public class findTargetIndex {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 55, 6};
+        int[] arr = {1, 2, 2, 3, 55, 6, 7, 7, 8, 8};
         System.out.println(find(arr, 55, 3));
         System.out.println(findIndex(arr, 55, 0));
+
+//        findAllIndex(arr, 7, 0);
+//        System.out.println(list);
+
+        System.out.println(findAllIndex2(arr, 8, 0, list));
+        System.out.println(findAllIndex3(arr, 8, 0));
     }
 
+    // return true or false
     static boolean find(int[] arr, int target, int index){
         // base condition
         if(index == arr.length){
@@ -14,6 +24,7 @@ public class findTargetIndex {
         return arr[index] == target || find(arr, target, index+1);
     }
 
+    // find the target index no.
     static int findIndex(int[] arr, int target, int index){
         if (index == arr.length){
             return -1;
@@ -23,5 +34,45 @@ public class findTargetIndex {
         } else {
             return findIndex(arr, target, index+1);
         }
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void  findAllIndex(int[] arr, int target, int index){
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target){
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+    }
+
+    static ArrayList<Integer> findAllIndex2(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if(index == arr.length){
+            return list;
+        }
+        if (arr[index] == target){
+            list.add(index);
+        }
+        return findAllIndex2(arr, target, index+1, list);
+    }
+
+    static ArrayList<Integer> findAllIndex3(int[] arr, int target, int index) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        if(index == arr.length){
+            return list1;
+        }
+
+        // for that particular func. call
+        if (arr[index] == target){
+            list1.add(index);
+        }
+        ArrayList<Integer> addAllAns = findAllIndex2(arr, target, index+1, list1);
+
+        list1.addAll(addAllAns);
+
+        return list1;
     }
 }
