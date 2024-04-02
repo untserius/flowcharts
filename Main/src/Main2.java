dcf4675ef00aa1e054ff5eb49fd4a79f9575393a
Sudhir Das <sudhirdas1999@gmail.com>
2024-04-02 19:00:54 +0530

    Added

--- Main/src/Main2.java
+++ Main/src/Main2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main2 {
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        /*Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();7
+
+        Test Test = new Test();
+
+        System.out.println(Test.method(n));*/
+
+        int x=100;
+        char z = (char)x;
+
+        System.out.println();
+    }
 }
--- Main/src/Test.java
+++ Main/src/Test.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public int method(int n) {
+        if (n == 0) return 1;
+        if (n < 0) n = -n;
+
+        int res = 0;
+
+        while (n != 0){
+            n = n / 10;
+            res++;
+        }
+        return res;
+    }
 }
